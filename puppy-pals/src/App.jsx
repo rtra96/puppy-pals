f3788b16557e3c979f4b1d6116ef3b1b9f514e92
Rick Trahant <rtra96@gmail.com>
2023-11-12 19:57:49 -0600

    refactor starting point. clean slate

--- puppy-pals/src/App.jsx
+++ puppy-pals/src/App.jsx
@@ -1,25 +1,5 @@
-import { useState } from 'react';
-import { puppyList } from './data';
-import './App.css'
 
-function App() {
-  const [puppies, setPuppies] = useState(puppyList);
-  const [featPupId, setfeatPupId] = useState(null);
-  const featuredPup = puppies.find((puppy) => puppy.id === featPupId);
- 
-  console.log (puppyList);
-  return <div>
-         {featPupId && (
-        <div>
-          <h2>{featuredPup.name}</h2>
-          <ul>
-            <li>Age: {featuredPup.age}</li>
-            <li>Email: {featuredPup.email}</li>
-          </ul>
-        </div>
-      )}
-    {puppies.map(puppy => 
-     (<p onClick={()=> setfeatPupId(puppy.id)} key={puppy.id}>{puppy.name}</p>))}</div>;
-}
+ <div>
+</div>;
 
-export default App;
+
